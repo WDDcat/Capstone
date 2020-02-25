f08e01f871bf1931b4b417615438daf35c62471f
wangshiyu <syyx20046@126.com>
2020-02-25 22:05:35 +0800

    start project

---
+++ Code/CapStoneProject/CapStoneProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapStoneProject
+//
+//  Created by Shiyu Wang on 2020/2/25.
+//  Copyright © 2020 Shiyu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
